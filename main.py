a5babbc0410e8c98e4806db15b7ed19368abd9c1
JuanDavid <juan@conchalabs.com>
2022-02-04 11:40:59 -0500

    Added logic for hotkeys in macos

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ from dateutil import parser
 from pynput import keyboard
 from todoist.api import TodoistAPI
 import pync
-import os
+import threading
 
 api = TodoistAPI('3ecd6c78d4e7a9548a4e4823807850906e8f6650')
 
 notified = []
-
 
 iterations = 0
 global snooze
@@ -25,6 +24,31 @@ def silent_notifications():
         snooze = True
 
 
+def check_shortcuts():
+    COMBINATIONS = [
+        {keyboard.Key.cmd, keyboard.Key.ctrl, keyboard.KeyCode(char='l')},
+        {keyboard.Key.cmd, keyboard.Key.ctrl, keyboard.KeyCode(char='L')}
+    ]
+
+    current = set()
+
+    def on_press(key):
+        if any([key in COMBO for COMBO in COMBINATIONS]):
+            current.add(key)
+            if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS):
+                silent_notifications()
+
+    def on_release(key):
+        if any([key in COMBO for COMBO in COMBINATIONS]):
+            current.remove(key)
+
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+
+
+t1 = threading.Thread(target=check_shortcuts, name='t1')
+t1.start()
+
 # add_hotkey('ctrl+alt+f5', silent_notifications)
 
 while 1:
@@ -36,7 +60,7 @@ while 1:
 
     api.sync()
 
-    # print(len(api.state['day_orders']))
+    print(len(api.state['day_orders']))
     for item in api.state['items']:
         try:
             due_date = item['due']['date']
@@ -46,11 +70,14 @@ while 1:
                 due_date = due_date.replace(hour=23, minute=59)
 
             if (due_date - timedelta(minutes=5)) <= today and not item['checked'] and {item['content'],
-                                                                                       item['due']['date']} not in notified and not snooze:
+                                                                                       item['due'][
+                                                                                           'date']} not in notified and not snooze:
                 notified.append({item['content'], item['due']['date']})
-                pync.notify("Hi", execute='say "OMG"', title='Todoist Reminders')
-                # notification.show_toast(f"{item['content']} Task is next", "Disable notifications with ctrl+alt+f5", duration=10,
-                #                         icon_path=r"C:\Users\JuanD\Desktop\Instant\todosit_remiders\res\todoist_logo.ico")
+                pync.notify(f"{item['content']} Task is next", "Disable notifications with cmd+ctrl+l",
+                            title='Todoist Reminders')
+                # notification.show_toast(f"{item['content']} Task is next", "Disable notifications with
+                # ctrl+alt+f5", duration=10,
+                # icon_path=r"C:\Users\JuanD\Desktop\Instant\todosit_remiders\res\todoist_logo.ico")
 
         except Exception as e:
             pass
